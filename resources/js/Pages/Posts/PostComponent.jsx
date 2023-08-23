7ca23bf46621506e89df7fbed35c87bff4e5d1bf
Sergei <qaserge@gmail.com>
2023-08-22 21:25:02 -0400

    react PostComponent and posts route added

---
+++ resources/js/Pages/Posts/PostComponent.jsx
@@ -0,0 +1,5 @@
+export default function PostComponent() {
+  return (
+    <div>PostComponent</div>
+  )
+}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/posts', function(){
+    return Inertia::render('Posts/PostComponent');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
