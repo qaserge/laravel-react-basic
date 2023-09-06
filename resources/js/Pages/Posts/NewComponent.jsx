8a535fbc84e10b01b8bc5e7dfad7342eaa21821a
Sergei <qaserge@gmail.com>
2023-09-06 12:36:36 -0400

    React set state

--- resources/js/Pages/Posts/NewComponent.jsx
+++ resources/js/Pages/Posts/NewComponent.jsx
@@ -9,16 +9,25 @@ export default class NewComponent extends Component {
     skills: ['react', 'laravel']
   }
 
-  handleClick = (e) => {
+  handleClick = () => {
     console.log('Button Clicked');
   }
 
   handleMouseOver = (e) => {
     console.log(e.nativeEvent.clientX, e.nativeEvent.clientY);
   }
-  
+
   handleCopy = (e) => {
     console.log('p has been copied');
+  }
+
+  handleChangeState = () => {
+    this.setState({
+      name: 'John',
+      age: 35,
+      mobile: 5143333333,
+      skills: ['PHP', 'Java']
+    })
   }
 
   render() {
@@ -30,14 +39,16 @@ export default class NewComponent extends Component {
         <p>Age: {this.state.age}</p>
         <p>Mobile: {this.state.mobile}</p>
         <p>Skills: {this.state.skills.join(', ')}</p>
-        <hr></hr>
-        <button
-          type="button"
-          onClick={this.handleClick}
-          onMouseOver={this.handleMouseOver}
-        >Click me</button>
+        <hr />
+        <button type="button" onClick={this.handleClick} onMouseOver={this.handleMouseOver}>Over me, then Click me and see the log</button>
 
-        <p onCopy={this.handleCopy}>Copy test</p>
+        <hr />
+
+        <p onCopy={this.handleCopy}>Copy this text and see the console log</p>
+
+        <hr />
+
+        <button type="button" onClick={this.handleChangeState} >Change State</button>
       </>
 
     )
