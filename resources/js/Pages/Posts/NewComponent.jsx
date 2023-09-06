d54d84adf1a216f80997fd58063d01f47412d3f6
Sergei <qaserge@gmail.com>
2023-09-06 12:27:36 -0400

    Dom event handler tests

--- resources/js/Pages/Posts/NewComponent.jsx
+++ resources/js/Pages/Posts/NewComponent.jsx
@@ -9,6 +9,18 @@ export default class NewComponent extends Component {
     skills: ['react', 'laravel']
   }
 
+  handleClick = (e) => {
+    console.log('Button Clicked');
+  }
+
+  handleMouseOver = (e) => {
+    console.log(e.nativeEvent.clientX, e.nativeEvent.clientY);
+  }
+  
+  handleCopy = (e) => {
+    console.log('p has been copied');
+  }
+
   render() {
     return (
       <>
@@ -18,6 +30,14 @@ export default class NewComponent extends Component {
         <p>Age: {this.state.age}</p>
         <p>Mobile: {this.state.mobile}</p>
         <p>Skills: {this.state.skills.join(', ')}</p>
+        <hr></hr>
+        <button
+          type="button"
+          onClick={this.handleClick}
+          onMouseOver={this.handleMouseOver}
+        >Click me</button>
+
+        <p onCopy={this.handleCopy}>Copy test</p>
       </>
 
     )
