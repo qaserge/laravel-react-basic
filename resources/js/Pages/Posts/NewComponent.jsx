5a6d4dceb6f2413cb1b33e7e1982ec52f7c8e2eb
Sergei <qaserge@gmail.com>
2023-09-06 12:14:03 -0400

    React state

--- resources/js/Pages/Posts/NewComponent.jsx
+++ resources/js/Pages/Posts/NewComponent.jsx
@@ -1,9 +1,25 @@
 import { Component } from "react";
 
 export default class NewComponent extends Component {
+
+  state = {
+    name: 'Bill',
+    age: 65,
+    mobile: 5145555555,
+    skills: ['react', 'laravel']
+  }
+
   render() {
     return (
-      <div>Class Component</div>
+      <>
+        <div>Class Component</div>
+
+        <p>Name: {this.state.name}</p>
+        <p>Age: {this.state.age}</p>
+        <p>Mobile: {this.state.mobile}</p>
+        <p>Skills: {this.state.skills.join(', ')}</p>
+      </>
+
     )
   }
 }
